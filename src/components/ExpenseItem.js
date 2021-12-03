371e9f5b16797d796b9a07222543ffb93c056e7f
syedahalam <syedahalam@yahoo.com>
2021-12-02 22:55:56 -0500

    start

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h2>Expense item in there!</h2>
+        </div>
+    )
+}
+
+export default ExpenseItem
